644c210bcbec077ab78aab29bbd845e51f9c21d1
Dillon Davis <07734willy@users.noreply.github.com>
2021-03-03 02:08:15 -0600

    a couple more tests

---
+++ tests/data/multiline/backslash/input.py
@@ -0,0 +1,33 @@
+
+
+x = \
+	2
+
+x
+
+y = \
+3 and \
+4 and \
+5
+
+y
+
+z = \
+	3 \
+and 4 \
+	and 5\
+and 6 \
+	and 7
+
+z
+
+q = 3 \
+-2
+
+q
+
+
+s = "foo " \
+"bar"
+
+s
---
+++ tests/data/multiline/backslash/output.py
@@ -0,0 +1,38 @@
+
+
+x = \
+	2
+
+x
+# out: 2
+
+y = \
+3 and \
+4 and \
+5
+
+y
+# out: 5
+
+z = \
+	3 \
+and 4 \
+	and 5\
+and 6 \
+	and 7
+
+z
+# out: 7
+
+q = 3 \
+-2
+
+q
+# out: 1
+
+
+s = "foo " \
+"bar"
+
+s
+# out: 'foo bar'
