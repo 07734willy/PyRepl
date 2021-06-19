34e8f413a58d48887fcc3d725b42d8968dbdafa0
Dillon Davis <07734willy@users.noreply.github.com>
2021-06-18 21:22:20 -0400

    parameterized performance tests

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,31 +1,34 @@
 from pyrepl.segments import get_segments
 import pytest
 
+REPEATS = 1000
 
 @pytest.mark.timeout(0.1)
 def test_parsing_many_statements():
 	statement = "x = 2"
+	repeats = REPEATS // 5
 
-	code = "\n".join([statement] * 1000)
+	code = "\n".join([statement] * repeats)
 
 	segments = get_segments(code)
-	assert len(segments) == 1000
+	assert len(segments) == repeats
 
 
 @pytest.mark.timeout(0.1)
 def test_parsing_many_unique_statements():
 	statement = "x = {}"
+	repeats = REPEATS // 5
 
-	code = "\n".join(statement.format(i) for i in range(1000))
+	code = "\n".join(statement.format(i) for i in range(repeats))
 
 	segments = get_segments(code)
-	assert len(segments) == 1000
+	assert len(segments) == repeats
 
 
 @pytest.mark.timeout(0.1)
 def test_parsing_construct_statements():
 	if_statement = "if True:"
-	statements = ["\tx = 3"] * 1000
+	statements = ["\tx = 3"] * REPEATS
 
 	code = if_statement + "\n" + "\n".join(statements)
 
@@ -36,7 +39,7 @@ def test_parsing_construct_statements():
 @pytest.mark.timeout(0.1)
 def test_parsing_else_statements():
 	begin_stmt = "if True:\n\tpass\nelse:"
-	statements = ["\tx = 3"] * 1000
+	statements = ["\tx = 3"] * REPEATS
 
 	code = begin_stmt + "\n" + "\n".join(statements)
 
@@ -49,7 +52,7 @@ def test_parsing_elif_ladder():
 	begin_stmt = "if True:\n\tx = 4"
 	middle_stmt = "elif False:\n\tx = 4"
 
-	code = "\n".join([begin_stmt] + [middle_stmt] * 300)
+	code = "\n".join([begin_stmt] + [middle_stmt] * (REPEATS // 3))
 
 	segments = get_segments(code)
 	assert len(segments) == 1
@@ -58,7 +61,7 @@ def test_parsing_elif_ladder():
 @pytest.mark.timeout(0.1)
 def test_long_trailing_multiline_string():
 	begin_stmt = "if True:\n\tx = 4"
-	middle_stmt = "'''" + "\n" * 1000 + "'''"
+	middle_stmt = "'''" + "\n" * REPEATS + "'''"
 	end_stmt = "x = 5"
 
 	code = "\n".join([begin_stmt] + [middle_stmt] + [end_stmt])
@@ -73,7 +76,7 @@ def test_long_list():
 	middle_stmt = "2,"
 	end_stmt = "]"
 
-	code = "\n".join([begin_stmt] + [middle_stmt] * 1000 + [end_stmt])
+	code = "\n".join([begin_stmt] + [middle_stmt] * REPEATS + [end_stmt])
 
 	segments = get_segments(code)
 	assert len(segments) == 1
@@ -85,7 +88,7 @@ def test_many_backslash_lines():
 	middle_stmt = "3 + \\"
 	end_stmt = "2\nx = 5"
 
-	code = "\n".join([begin_stmt] + [middle_stmt] * 1000 + [end_stmt])
+	code = "\n".join([begin_stmt] + [middle_stmt] * REPEATS + [end_stmt])
 
 	segments = get_segments(code)
 	assert len(segments) == 3
@@ -97,7 +100,7 @@ def test_multiline_parenthesized_expression():
 	middle_stmt = "3 and "
 	end_stmt = "2)\nx = 5"
 
-	code = "\n".join([begin_stmt] + [middle_stmt] * 1000 + [end_stmt])
+	code = "\n".join([begin_stmt] + [middle_stmt] * REPEATS + [end_stmt])
 
 	segments = get_segments(code)
 	assert len(segments) == 3
