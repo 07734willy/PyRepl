913de93366eebb401422c3fe443a883d14a49e8c
07734willy <07734willy@users.noreply.github.com>
2022-03-17 08:48:03 -0400

    added forgotten test data file

---
+++ tests/data/errors/incomplete_construct/output-3-8.py
@@ -0,0 +1,54 @@
+
+if True:
+	pass
+else:
+	pass
+elif False:
+	pass
+# error:   File "<string>", line 6
+# error:     elif False:
+# error:     ^
+# error: SyntaxError: invalid syntax
+
+
+
+try:
+	pass
+finally:
+	pass
+except:
+	pass
+# error:   File "<string>", line 19
+# error:     except:
+# error:     ^
+# error: SyntaxError: invalid syntax
+
+x = 1
+
+except:
+	pass
+# error:   File "<string>", line 28
+# error:     except:
+# error:     ^
+# error: SyntaxError: invalid syntax
+
+x = 2
+
+else:
+	pass
+# error:   File "<string>", line 37
+# error:     else:
+# error:     ^
+# error: SyntaxError: invalid syntax
+
+
+def decor(func):
+	return func
+
+
+@decor
+x = 2
+# error:   File "<string>", line 50
+# error:     x = 2
+# error:     ^
+# error: SyntaxError: invalid syntax
